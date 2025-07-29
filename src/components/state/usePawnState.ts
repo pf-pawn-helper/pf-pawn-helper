41e0fd489a0c27d2456bfd476e579b818b6f3766
Jason Pattle <jasonpattle90@gmail.com>
2025-07-29 12:18:06 +0100

    feat: replaced local state for pawn name and pawn names with use of query params

--- src/components/state/usePawnState.ts
+++ src/components/state/usePawnState.ts
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useQueryParam, useQueryParamArray } from "../../hooks/useQueryParams";
 
 export const usePawnState = () => {
-  const [pawnName, setPawnName] = useState<string>("");
-  const [pawnNames, setPawnNames] = useState<string[]>([]);
+  const [pawnName, setPawnName] = useQueryParam("p", ""); // "p" for pawn (singular); reduces url length
+  const [pawnNames, setPawnNames] = useQueryParamArray("l", []); // "l" for list (of pawns); reduces url length
   return { pawnName, setPawnName, pawnNames, setPawnNames };
 };
