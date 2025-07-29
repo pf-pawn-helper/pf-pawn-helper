e33af5fec8ef27c014756f3a6cb81b75cdb49578
Jason Pattle <jasonpattle90@gmail.com>
2025-07-29 12:14:38 +0100

    feat: updated use query params to allow for array of values

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -1,37 +1,64 @@
 import { useState } from "react";
 
+const windowIsDefined = () => typeof window !== "undefined";
+
 const getQuery = () => {
-  if (typeof window !== "undefined") {
+  if (windowIsDefined()) {
     return new URLSearchParams(window.location.search);
   }
   return new URLSearchParams();
 };
 
-const getQueryStringVal = (key: string): string | null => {
-  return getQuery().get(key);
-};
+const getQueryStringValues = (key: string): string[] => getQuery().getAll(key);
 
 export const useQueryParam = (
   key: string,
-  defaultVal: string
-): [string, (val: string) => void] => {
-  const [query, setQuery] = useState(getQueryStringVal(key) || defaultVal);
+  defaultValue: string | undefined
+): [string, (newValue: string) => void] => {
+  const [query, setQuery] = useQueryParamArray(
+    key,
+    defaultValue && defaultValue.trim() !== "" ? [defaultValue] : []
+  );
 
-  const updateUrl = (newVal: string) => {
-    setQuery(newVal);
+  const updateUrl = (newValue: string | undefined) =>
+    setQuery(newValue && newValue.trim() !== "" ? [newValue] : []);
+
+  return [query[0] ?? "", updateUrl];
+};
+
+export const useQueryParamArray = (
+  key: string,
+  defaultValues: string[]
+): [string[], (newValues: string[]) => void] => {
+  const [query, setQuery] = useState(
+    getQueryStringValues(key).length > 0
+      ? getQueryStringValues(key)
+      : defaultValues
+  );
+
+  const updateUrl = (newValues: string[]) => {
+    setQuery(newValues);
 
     const query = getQuery();
+    console.log("updating query", query);
 
-    if (newVal.trim() !== "") {
-      query.set(key, newVal);
+    if (newValues.length > 0) {
+      newValues.forEach((value, index) => {
+        if (index === 0) {
+          query.set(key, value);
+        } else {
+          query.append(key, value);
+        }
+      });
     } else {
       query.delete(key);
     }
 
-    // This check is necessary if using the hook with Gatsby
-    if (typeof window !== "undefined") {
+    if (windowIsDefined()) {
       const { protocol, pathname, host } = window.location;
-      const newUrl = `${protocol}//${host}${pathname}?${query.toString()}`;
+      const queryString = query.toString().trim();
+      const queryParams = queryString === "" ? "" : `?${queryString}`;
+      const newUrl = `${protocol}//${host}${pathname}${queryParams}`;
       window.history.pushState({}, "", newUrl);
     }
   };
